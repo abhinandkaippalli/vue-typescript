1733de22f92216136873ef0accd0b133657a2128
Abhinand <abhinandkofficial01@gmail.com>
2023-03-29 21:37:25 +0530

    computed property

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -3,7 +3,7 @@
     <div class="job-list">
       <p>Ordered by {{ order }}</p>
       <ul>
-        <li v-for="job in jobs" :key="job.id">
+        <li v-for="job in orderedJobs" :key="job.id">
           <h2>{{ job.title }} in {{ job.location }}</h2>
           <div class="salary">
             <p>{{ job.salary }} rupees</p>
@@ -23,7 +23,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, computed } from "vue";
 import Job from "@/types/job";
 import OrderTerm from "@/types/OrderTerm";
 
@@ -38,6 +38,16 @@ export default defineComponent({
       type: String as PropType<OrderTerm>
     }
   },
+
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1
+      })
+    })
+
+    return { orderedJobs }
+  }
 });
 </script>
 
